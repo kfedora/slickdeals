b996af0faae7162828efff264ec8796d98bc8a43
Linus Torvalds <torvalds@linux-foundation.org>
2020-10-18 09:43:50 +0000

    add all files
    yes, really

--- slickdeals.net/forums/sendmessage.php
+++ slickdeals.net/forums/sendmessage.php
@@ -2,10 +2,11 @@ HTTP/1.1 500 Internal Server Error
 Cache-Control: no-cache
 Content-Type: text/html
 Content-Length: 96
-Date: Sun, 18 Oct 2020 09:00:01 GMT
+X-Akamai-Path-Stats: [3:7057:14999943]
+Date: Sun, 18 Oct 2020 06:31:08 GMT
 Connection: keep-alive
 Server-Timing: cdn-cache; desc=MISS
-Server-Timing: edge; dur=64
+Server-Timing: edge; dur=33
 Server-Timing: origin; dur=15007
 Strict-Transport-Security: max-age=86400
 
